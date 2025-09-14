8f9c0e70fb9d6af3e6e394f09de2c5c93e0c883d
a.k.a fl1p <bishu.hover@gmail.com>
2025-09-15 05:12:48 +0800

    fist

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef } from "react";
+import { cn } from "../lib/cn";
+import gsap from "gsap";
+const AnimatedTitle = ({ title, containerClass }) => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      const titleAnimation = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "100 bottom",
+          end: "center bottom",
+          toggleActions: "play none none reverse",
+          // scrub: true,
+        },
+      });
+      
+      titleAnimation.to('.animated-word', {
+       opacity: 1,
+       transform: 'translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)',
+
+       ease: "power2.inOut",
+       stagger: 0.02,
+      })
+    }, containerRef);
+
+    return () => ctx.revert();
+  }, []);
+
+  return (
+    <div ref={containerRef} className={cn("animated-title", containerClass)}>
+      {title.split("<br />").map((line, index) => (
+        <div key={index} className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3">
+          {line.split(" ").map((word, idx) => (
+            <span key={idx} className="special-font font-zentry animated-word font-light!" dangerouslySetInnerHTML={{ __html: word }} />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnimatedTitle;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { FaDiscord, FaTwitter, FaGithub, FaTwitch } from "react-icons/fa";
+
+const links = [
+  { icon: <FaDiscord />, href: "https://discord.com" },
+  { icon: <FaTwitter />, href: "https://twitter.com" },
+  { icon: <FaGithub />, href: "https://github.com" },
+  { icon: <FaTwitch />, href: "https://twitch.com" },
+];
+const Footer = () => {
+  return (
+    <footer className="w-screen bg-violet-300 py-4 text-black">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+        <p className="text-center text-sm md:text-left">&copy; Nova 2024. All rights reserved</p>
+        <div className="flex justify-center gap-4 md:justify-start">
+          {links.map((link) => (
+            <a key={link} href={link.href} target="_blank" rel="noopener noreferrer" className="text-black transition-colors ease-in-out duration-500 hover:text-white">
+              {link.icon}
+            </a>
+          ))}
+        </div>
+
+        <a href="#privacy-policy" className="text-center text-sm hover:underline md:text-right">Privacy Policy</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
